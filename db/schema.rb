e3755b7c0acd40c33f3e05b4fa8466fee57c747e
Hansol Jeong <hansol89j@gmail.com>
2021-11-21 01:07:53 +0900

    Created login page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_064630) do
+ActiveRecord::Schema.define(version: 2021_11_19_134652) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2021_11_18_064630) do
     t.string "title"
     t.text "description"
     t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
